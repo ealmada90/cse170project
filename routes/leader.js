94025a26608db3d42de41f2b8b1f7308a8813243
nanoracket <nanoracket@gmail.com>
2016-03-03 01:04:50 -0800

    fixed points

--- routes/leader.js
+++ routes/leader.js
@@ -7,9 +7,18 @@ exports.view = function(req, res) {
 	var name = req.query.usr;
 	var email = req.query.eml;
 	var tm = req.query.tm;
+
+	var hero = 0;
+	var vill = 0;
 	for(i =0; i < user["users"].length; i++){
 		if(name == user["users"][i]['name'] && email == user["users"][i]['email']){
 			index = i;
+		}
+		if(user["users"][i]['team'] =="Heroes"){
+			hero +=user["users"][i]['points'];
+		}
+		else if(user["users"][i]['team'] =="Villains"){
+			vill +=user["users"][i]['points'];
 		}
 	}
 	if(index == -1){
@@ -20,6 +29,16 @@ exports.view = function(req, res) {
 	data["tm"] = user["users"][index]["team"];
 	data["email"] = user["users"][index]["email"];
 	data["user"] = user["users"][index]["name"];
+	if(hero >= vill){
+		data["board"] = [{"team":"Heroes", "points": hero},{"team":"Villains", "points": vill}];
+	}
+	else{
+		data["board"] = [{"team":"Villains", "points": vill},{"team":"Heroes", "points": hero}];
+	}
+
+	
+
+
     //console.log("hi");
 	res.render('leaderboard',data)
 }
